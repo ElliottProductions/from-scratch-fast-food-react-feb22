7c638a2d28064d3292c7ccf16e255609b6bec9a6
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-05-03 15:00:14 -0800

    added images and and wrote Meal component to display them

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import OrderForm from './OrderForm';
 import DropDown from './DropDown';
+import Meal from './Meal';
 
 import { useState } from 'react';
 
@@ -10,13 +11,16 @@ import { useState } from 'react';
 function App() {
 
   const [orderName, setOrderName] = useState('Customer #001');
-  const [drinkChoice, setDrinkChoice] = useState(1);
+  const [drinkID, setDrinkId] = useState(1);
+  const [sideId, setSideId] = useState(1);
+  const [foodId, setFoodId] = useState(1);
   return (
     <div>
       <h3>Order for {orderName}</h3>
-      <DropDown setChoice={setDrinkChoice} itemName={'drink'}/>
-      <DropDown setChoice={setDrinkChoice} itemName={'entrée'}/>
-      <DropDown setChoice={setDrinkChoice} itemName={'side'}/>
+      <Meal drinkId={drinkID} sideId={sideId} foodId={foodId}/>
+      <DropDown setChoice={setDrinkId} itemName={'drink'}/>
+      <DropDown setChoice={setFoodId} itemName={'entrée'}/>
+      <DropDown setChoice={setSideId} itemName={'side'}/>
       <OrderForm setOrderName={setOrderName}/>
     </div>
     
